26e62a8ba2424a0d2b172b67b4681737c30c07cd
Reese Avery Kling <raveryk@gmail.com>
2021-04-19 18:39:12 -0500

    Got up to and through updating GET routes on Server side. Got everything displayed on DOM but the date is displaying with extra characters."  ..

--- database.sql
+++ database.sql
@@ -1,5 +1,29 @@
-CREATE TABLE "artists" (
-    "id" SERIAL PRIMARY KEY,
-    "artist_name" varchar(80) not null,
-    "year_born" date
-);
+-- CREATE TABLE "artists" (
+--     "id" SERIAL PRIMARY KEY,
+--     "artist_name" varchar(80) not null,
+--     "year_born" date
+-- );
+
+CREATE TABLE "artist" (
+	"id" serial primary key,
+	"name" varchar(80),
+	"birthdate" date
+	);
+	
+CREATE TABLE "song" (
+	"id" serial primary key,
+	"title" varchar(255),
+	"length" varchar(10),
+	"released" date
+	);
+	
+INSERT INTO "artist" ("id", "name", "birthdate")
+VALUES (1, 'Ella Fitzgerald', '04-25-1917'),
+(2, 'Dave Brubeck', '12-06-1920'),
+(3, 'Miles Davis', '05-26-1926'),
+(4, 'Esperanza Spalding', '10-18-1984');
+
+INSERT INTO "song" ("id", "title", "length", "released")
+VALUES (1, 'Take Five', '5:24','1959-09-29'),
+(2, 'So What','9:22','1959-08-17'),
+(3, 'Black Gold','5:17','2012-02-01');
--- server/server.js
+++ server/server.js
@@ -11,46 +11,74 @@ app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
 
-// TODO - Replace static content with a database tables
-const artistList = [ 
-    {
-        name: 'Ella Fitzgerald',
-        birthdate: '04-25-1917'
-    },
-    {
-        name: 'Dave Brubeck',
-        birthdate: '12-06-1920'
-    },       
-    {
-        name: 'Miles Davis',
-        birthdate: '05-26-1926'
-    },
-    {
-        name: 'Esperanza Spalding',
-        birthdate: '10-18-1984'
-    },
-]
-const songList = [
-    {
-        title: 'Take Five',
-        length: '5:24',
-        released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        released: '1959-08-17'
-    },
-    {
-        title: 'Black Gold',
-        length: '5:17',
-        released: '2012-02-01'
-    }
-];
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool ({
+    database: 'jazzy_sql',
+    host: 'localHost',
+    port: '5432',
+    max: 10,
+    idleTimeoutMillis: 30000
+});
 
+pool.on('connect', () => {
+    console.log('Postgresql connected!!! Woott!!');
+});
+
+pool.on('error', error => {
+    console.log('Error with postgres pool', error)
+});
+
+// TODO - Replace static content with a database tables
+// const artistList = [ 
+//     {
+//         name: 'Ella Fitzgerald',
+//         birthdate: '04-25-1917'
+//     },
+//     {
+//         name: 'Dave Brubeck',
+//         birthdate: '12-06-1920'
+//     },       
+//     {
+//         name: 'Miles Davis',
+//         birthdate: '05-26-1926'
+//     },
+//     {
+//         name: 'Esperanza Spalding',
+//         birthdate: '10-18-1984'
+//     },
+// ]
+// const songList = [
+//     {
+//         title: 'Take Five',
+//         length: '5:24',
+//         released: '1959-09-29'
+//     },
+//     {
+//         title: 'So What',
+//         length: '9:22',
+//         released: '1959-08-17'
+//     },
+//     {
+//         title: 'Black Gold',
+//         length: '5:17',
+//         released: '2012-02-01'
+//     }
+// ];
+
+// Retreiving artist data from the database
 app.get('/artist', (req, res) => {
     console.log(`In /songs GET`);
-    res.send(artistList);
+    // 
+    let queryText = 'SELECT * FROM "artist" ORDER BY "birthdate" DESC;';
+    pool.query(queryText)
+        .then(dbResult => {
+            res.send(dbResult.rows);
+        })
+        .catch(error => {
+            console.log(`Error! It broke trying to query ${queryText}`, error);
+            res.sendStatus(500);    
+        })
 });
 
 app.post('/artist', (req, res) => {
@@ -58,9 +86,19 @@ app.post('/artist', (req, res) => {
     res.sendStatus(201);
 });
 
+//Retreiving song data from the Database
 app.get('/song', (req, res) => {
     console.log(`In /songs GET`);
-    res.send(songList);
+    // res.send(songList);
+    let queryText = 'SELECT * FROM "song" ORDER BY "title";';
+    pool.query(queryText)
+        .then(dbResult => {
+            res.send(dbResult.rows);
+        })
+        .catch(error => {
+            console.log(`Error! It broke trying to query ${queryText}`, error);
+            res.sendStatus(500);    
+        })
 });
 
 app.post('/song', (req, res) => {
